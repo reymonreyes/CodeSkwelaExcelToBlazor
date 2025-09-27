6d95812ccc309169aef7da4367723939389d51ca
Reymon Reyes <elmoe01@gmail.com>
2025-09-27 11:31:33 +0800

    Created initial migration for the Provider model.
    - Run Add-Migration and Update-Database commands to create the sqlite database and Providers table.
    - Inspect database using Db Browser.

---
+++ SimpleServiceInvoice/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleServiceInvoice.Models;
+
+namespace SimpleServiceInvoice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private string DbPath;
+        public DbSet<Provider> Providers { get; set; }
+
+        public ApplicationDbContext()
+        {
+            var folder = Environment.CurrentDirectory;
+            DbPath = System.IO.Path.Join(folder, "Data\\application.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+}
---
+++ SimpleServiceInvoice/Migrations/20250927001133_CreateProvidersTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleServiceInvoice.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateProvidersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Providers");
+        }
+    }
+}
