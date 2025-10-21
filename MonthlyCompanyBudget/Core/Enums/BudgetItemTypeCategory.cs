9066c866684744429ac54d606a2543e5c38b98e8
Reymon Reyes <elmoe01@gmail.com>
2025-10-21 19:12:54 +0800

    Migration for creating the BudgetItemTypes table.

---
+++ MonthlyCompanyBudget/Core/Enums/BudgetItemTypeCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace MonthlyCompanyBudget.Core.Enums
+{
+    public enum BudgetItemTypeCategory
+    {
+        Income = 1,
+        PersonnelExpenses = 2,
+        OperatingExpenses = 3
+    }
+}
---
+++ MonthlyCompanyBudget/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonthlyCompanyBudget.Core.Entities;
+
+namespace MonthlyCompanyBudget.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private string _dbPath;
+        public DbSet<BudgetItemType> BudgetItemTypes { get; set; }
+
+        public ApplicationDbContext()
+        {
+            _dbPath = Path.Join(Environment.CurrentDirectory, "Data\\application.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+        }
+    }
+}
