eb39fd808b280f0fd64aad0b9ad1a653c6c65ae5
Reymon Reyes <elmoe01@gmail.com>
2025-10-22 14:48:00 +0800

    Moved the Migrations folder to be under Data. Recreated the database by deleting it and running the Update-Database command again, this also added the seed data. Initial display of BudgetItemTypes data.

--- MonthlyCompanyBudget/Data/ApplicationDbContext.cs
+++ MonthlyCompanyBudget/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MonthlyCompanyBudget.Core.Entities;
+using MonthlyCompanyBudget.Core.Enums;
 
 namespace MonthlyCompanyBudget.Data
 {
@@ -15,7 +16,78 @@ namespace MonthlyCompanyBudget.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+            optionsBuilder.UseSqlite($"Data Source={_dbPath}")
+            .UseSeeding((context, _) =>
+            {
+                if (!context.Set<BudgetItemType>().Any())
+                {
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Net sales", Category = BudgetItemTypeCategory.Income });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Interest income", Category = BudgetItemTypeCategory.Income });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Asset sales(gain/ loss)", Category = BudgetItemTypeCategory.Income });
+
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Wages", Category = BudgetItemTypeCategory.PersonnelExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Employee benefits", Category = BudgetItemTypeCategory.PersonnelExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Commission", Category = BudgetItemTypeCategory.PersonnelExpenses });
+
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Advertising", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Bad debts", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Cash discounts", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Delivery costs", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Depreciation", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Dues and subscriptions", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Insurance", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Interest", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Legal and auditing", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Maintenance and repairs", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Office supplies", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Postage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Rent or mortgage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Sales expenses", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Shipping and storage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Supplies", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Taxes", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Telephone", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Utilities", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Other", Category = BudgetItemTypeCategory.OperatingExpenses });
+
+                    context.SaveChanges();
+                }
+            }).UseAsyncSeeding(async(context, _, cancellationToken) =>
+            {
+                if (!context.Set<BudgetItemType>().AnyAsync().Result)
+                {
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Net sales", Category = BudgetItemTypeCategory.Income });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Interest income", Category = BudgetItemTypeCategory.Income });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Asset sales(gain/ loss)", Category = BudgetItemTypeCategory.Income });
+
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Wages", Category = BudgetItemTypeCategory.PersonnelExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Employee benefits", Category = BudgetItemTypeCategory.PersonnelExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Commission", Category = BudgetItemTypeCategory.PersonnelExpenses });
+
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Advertising", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Bad debts", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Cash discounts", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Delivery costs", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Depreciation", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Dues and subscriptions", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Insurance", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Interest", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Legal and auditing", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Maintenance and repairs", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Office supplies", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Postage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Rent or mortgage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Sales expenses", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Shipping and storage", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Supplies", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Taxes", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Telephone", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Utilities", Category = BudgetItemTypeCategory.OperatingExpenses });
+                    context.Set<BudgetItemType>().Add(new BudgetItemType { Name = "Other", Category = BudgetItemTypeCategory.OperatingExpenses });
+
+                    await context.SaveChangesAsync(cancellationToken);
+                }
+            });
         }
     }
 }
--- MonthlyCompanyBudget/Data/Migrations/20251021115648_CreateBudgetItemTypesTable.cs
+++ MonthlyCompanyBudget/Data/Migrations/20251021115648_CreateBudgetItemTypesTable.cs
@@ -2,7 +2,7 @@
 
 #nullable disable
 
-namespace MonthlyCompanyBudget.Migrations
+namespace MonthlyCompanyBudget.Data.Migrations
 {
     /// <inheritdoc />
     public partial class CreateBudgetItemTypesTable : Migration
