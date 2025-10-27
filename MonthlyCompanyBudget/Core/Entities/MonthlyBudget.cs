b10a4b23c1fd8e6a3c0f4fcf42200e700b2eb99a
Reymon Reyes <elmoe01@gmail.com>
2025-10-27 20:27:38 +0800

    Functions for managing Invoice budget items.
    - Adding of a new month of budget items.
    - Save any changes to the items.
    - Filter the items by month and year. The default filter (when the page loads) is the latest month.
    - Improvements:
      - A function to remove an item.
      - A function to add an item.
      - A function to filter not on the filter options.
      - A validation that would prevent the user from adding an existing month.

--- MonthlyCompanyBudget/Core/Entities/MonthlyBudget.cs
+++ MonthlyCompanyBudget/Core/Entities/MonthlyBudget.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-        public List<MonthlyBudgetItem> Items { get; set; }
+        public List<MonthlyBudgetItem> Items { get; set; } = new List<MonthlyBudgetItem>();
     }
 
     public class MonthlyBudgetItem
@@ -12,7 +12,23 @@
         public int Id { get; set; }
         public int MonthlyBudgetId { get; set; }
         public int BudgetItemTypeId { get; set; }
+        public BudgetItemType BudgetItemType { get; set; }
         public decimal EstimatedValue { get; set; }
         public decimal ActualValue { get; set; }
+        public decimal Difference
+        {
+            get
+            {
+                return ActualValue - EstimatedValue;
+            }
+        }
+
+        public decimal Top5Amount 
+        {
+            get
+            {
+                return ActualValue + (decimal)(Math.Pow(10, -6) * Id);
+            }
+        }
     }
 }
