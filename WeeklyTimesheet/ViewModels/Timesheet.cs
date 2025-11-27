9a5ac230110802fd857f40db3ebb2a4a0b2317eb
Reymon Reyes <elmoe01@gmail.com>
2025-11-27 19:21:13 +0800

    3.3 Building the UI.
    - Time Sheets index page.
    - List of time sheets.
    - Filter options by Employee or Week Start.
    - Add Time Sheet via modal form.

---
+++ WeeklyTimesheet/ViewModels/Timesheet.cs
@@ -0,0 +1,10 @@
+﻿namespace WeeklyTimesheet.ViewModels
+{
+    public class Timesheet
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime WeekStart { get; set; }
+        public int TotalHours { get; set; }
+    }
+}
